e2ac6c3bec04b2e7df515c886ad8e9a0605a5786
VaishnaviLabade16 <vaishnavilabade7@gmail.com>
2026-01-03 14:14:55 +0530

    Add files via upload

---
+++ Info.sql
@@ -0,0 +1,6 @@
+show databases;
+use vaishnavi;
+create table info(id int, name varchar(50), department varchar(50)); 
+insert into info values(1,'vaishnavi','DA');
+insert into info values(2,'sakshi','IT');
+select *from info;
